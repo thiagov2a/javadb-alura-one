020971437f3510e2ca152e7ed138a9a2645df043
Thiago Vildosa <tvildosa@gmail.com>
2024-07-13 06:33:31 -0300

    ScreenMatch Sprint 1

--- screenmatch/src/main/java/com/thiagov2a/screenmatch/dto/EpisodioDTO.java
+++
@@ -1,6 +0,0 @@
-package com.thiagov2a.screenmatch.dto;
-
-public record EpisodioDTO(Integer temporada,
-                          String titulo,
-                          Integer numeroEpisodio) {
-}
